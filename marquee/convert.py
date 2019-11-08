c182a205d62f4a1dc43c0c52785c16742a188750
rinalim <46272802+rinalim@users.noreply.github.com>
2019-11-08 18:25:40 +0900

    Create convert.py

---
+++ marquee/convert.py
@@ -0,0 +1,19 @@
+import sys, os
+from subprocess import *
+
+def run_cmd(cmd):
+# runs whatever in the cmd variable
+    p = Popen(cmd, shell=True, stdout=PIPE)
+    output = p.communicate()[0]
+    return output
+  
+source_path = sys.argv[1]
+
+if os.path.isdir(source_path) == True:
+    file_list = os.listdir(source_path)
+    file_list.sort()
+    for f in file_list:
+        if ".png" in f:
+            run_cmd("convert " + source_path + f + " -resize 1024x600 ./" + f)
+            print "convert " + source_path + f + " -resize 1024x600 ./" + f
+

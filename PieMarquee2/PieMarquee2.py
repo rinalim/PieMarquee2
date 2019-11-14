1dff0aeb90fc23a9ee77e35b4f20fee2973db342
rinalim <46272802+rinalim@users.noreply.github.com>
2019-11-15 01:05:35 +0900

    Update PieMarquee2.py

--- PieMarquee2/PieMarquee2.py
+++ PieMarquee2/PieMarquee2.py
@@ -16,11 +16,14 @@ def run_cmd(cmd):
 def kill_proc(name):
     ps_grep = run_cmd("ps -aux | grep " + name + "| grep -v 'grep'")
     if len(ps_grep) > 1: 
-        run_cmd("killall -9 " + name)
+        os.system("killall " + name)
 
 if os.path.isfile(INTRO) == True:
     run_cmd("omxplayer --display 4 " + INTRO)
 
+os.system("omxiv /tmp/marquee.txt -d 4  -f -t 5 -T blend --duration 1000 &")    
+os.system("echo '/home/pi/PieMarquee/marquee/maintitle.png' > /tmp/marquee.txt")
+    
 cur_imgpath = ""
 change_count = 0
 while True:
@@ -60,15 +63,7 @@ while True:
         romname = "maintitle"
    
     if os.path.isfile("/home/pi/PieMarquee/marquee/" + romname + ".png") == True:
-        if len(ps_grep) > 1 and os.path.isfile("/home/pi/PieMarquee/marquee/" + romname + "-1.png") == True:
-            change_count = change_count+1
-            if change_count == CHANGE_INTERVAL:
-                if cur_imgpath == romname:
-                    imgpath = romname+"-1"
-                elif cur_imgpath == romname+"-1":
-                    imgpath = romname
-        else:
-            imgpath = romname
+        imgpath = romname
     elif os.path.isfile("/home/pi/PieMarquee/marquee/" + sysname + ".png") == True:
         imgpath = sysname
     else:
@@ -77,13 +72,12 @@ while True:
     #print romname
     if imgpath != cur_imgpath:
         #print imgpath 
-        kill_proc("pngview")
         kill_proc("omxplayer.bin")
         if imgpath == "maintitle" and os.path.isfile("/home/pi/PieMarquee/marquee/maintitle.mp4") == True:
             os.system("omxplayer --loop --no-osd --display 4 /home/pi/PieMarquee/marquee/maintitle.mp4 &")
         else:
-            os.system("/usr/bin/pngview -d4 /home/pi/PieMarquee/marquee/" + imgpath + ".png &")
-            #os.system("/usr/bin/pngview -l30000 -y0 /home/pi/PieMarquee/marquee/" + imgpath + ".png &")
+            fullpath = "/home/pi/PieMarquee/marquee/" + imgpath + ".png"
+            os.system("echo '" + fullpath + "' > /tmp/marquee.txt")
         cur_imgpath = imgpath
         change_count = 0
 

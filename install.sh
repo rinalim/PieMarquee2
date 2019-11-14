608ab1a93ed1a8bff633448c4b7c6b0e41cfa3f9
rinalim <46272802+rinalim@users.noreply.github.com>
2019-11-15 01:06:55 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,11 @@
-sudo cp ./libraspidmx.so.1 /usr/lib
-sudo cp ./pngview /usr/bin
-sudo chmod 755 /usr/bin/pngview
-
 sudo apt-get install imagemagick -y
 
-rm -rf /opt/retropie/configs/all/PieMarquee/
-mkdir /opt/retropie/configs/all/PieMarquee/
-cp -f -r ./PieMarquee /opt/retropie/configs/all/
+rm -rf /opt/retropie/configs/all/PieMarquee2/
+mkdir /opt/retropie/configs/all/PieMarquee2/
+cp -f -r ./PieMarquee2 /opt/retropie/configs/all/
 
-sudo sed -i '/PieMarquee.py/d' /opt/retropie/configs/all/autostart.sh
-sudo sed -i '1i\\/usr/bin/python /opt/retropie/configs/all/PieMarquee/PieMarquee.py &' /opt/retropie/configs/all/autostart.sh
+sudo sed -i '/PieMarquee2.py/d' /opt/retropie/configs/all/autostart.sh
+sudo sed -i '1i\\/usr/bin/python /opt/retropie/configs/all/PieMarquee2/PieMarquee2.py &' /opt/retropie/configs/all/autostart.sh
 
 echo
 echo "Setup Completed. Reboot after 3 Seconds."

7dfc5f934294da2a228c17a9404e11f9ec746392
rinalim <ooll00@naver.com>
2020-08-08 22:38:07 +0900

    Create pi3-extension.sh

---
+++ scripts/pi3-extension.sh
@@ -0,0 +1,10 @@
+killall emulationstation
+
+sudo cp /opt/retropie/supplementary/emulationstation/emulationstation /opt/retropie/supplementary/emulationstation/emulationstation_org
+sudo cp ./ES-pi3/emulationstation /opt/retropie/supplementary/emulationstation/emulationstation
+sudo chmod 755 /opt/retropie/supplementary/emulationstation/emulationstation
+
+echo
+echo "Setup Completed. Reboot after 3 Seconds."
+sleep 3
+reboot

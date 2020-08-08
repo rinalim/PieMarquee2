94a3c349aff71f46938bfc9952b7a3ea147ac0e7
rinalim <ooll00@naver.com>
2020-08-08 17:28:46 +0900

    extension patch

---
+++ scripts/pi4-extension.sh
@@ -0,0 +1,8 @@
+sudo cp /opt/retropie/supplementary/emulationstation/emulationstation /opt/retropie/supplementary/emulationstation/emulationstation_org
+sudo cp ./ES-pi4/emulationstation_org /opt/retropie/supplementary/emulationstation/emulationstation
+sudo chmod 755 /opt/retropie/supplementary/emulationstation/emulationstation
+
+echo
+echo "Setup Completed. Reboot after 3 Seconds."
+sleep 3
+reboot

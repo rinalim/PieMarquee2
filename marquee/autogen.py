d8e1795ea2bfe6b1dd23b22ee009219ff85de212
rinalim <46272802+rinalim@users.noreply.github.com>
2020-09-26 02:07:45 +0900

    Update autogen.py

--- marquee/autogen.py
+++ marquee/autogen.py
@@ -7,9 +7,11 @@ def run_cmd(cmd):
     output = p.communicate()[0]
     return output
   
+input_dir = sys.argv[1].split("/")[0]
+input_file = sys.argv[1].split("/")[1]
 source_path = "/home/pi/RetroPie/roms/"+sys.argv[1]+"/marquee/"
 snapshot_parh = "/home/pi/RetroPie/roms/"+sys.argv[1]+"/snap/"
-dest_path = sys.argv[1]
+dest_path = sys.argv[1]+"/"
 resize = sys.argv[2]
 resize_method = sys.argv[3]
 bg_color = sys.argv[4]
@@ -19,28 +21,29 @@ marquee_size = sys.argv[6]
 run_cmd("convert -size " + resize + "xc:" + bg_color + 
     " -matte -channel A +level 0," + bg_trans + "% +channel ./bg.png")
 
-if source_path.endswith("/") == False:
-    source_path = source_path+"/"
-if dest_path.endswith("/") == False:
-    dest_path = dest_path+"/"
-
 if os.path.isdir(source_path) == False:
     print "source path is not valid"
 else:
     if os.path.isdir(os.getcwd()+"/"+dest_path) == False:
         os.mkdir(os.getcwd()+"/"+dest_path)
-    file_list = os.listdir(source_path)
-    file_list.sort()
+    if input_file == "*":
+        file_list = os.listdir(source_path)
+    else:
+        file_list = [source_path+"/"+input_file]
     for f in file_list:
         if ".png" in f:
-            run_cmd('convert "' + source_path + f + '" -background black -alpha remove -resize ' + resize + ' "./' + dest_path + f + '"')
-            print 'convert "' + source_path + f + '" -background black -alpha remove -resize ' + resize + ' "./' + dest_path + f + '"'
+            run_cmd('convert "' + snapshot_parh + f + '" -resize ' + resize + ' "\! bg.png -composite' + dest_path + f + '"')
+            run_cmd('convert "' + source_path + f + '" -resize ' + resize + ' logo.png')
+            run_cmd('convert logo.png -background black -shadow 80x3+5+5 logo.png -composite shadow.png')
+            run_cmd('composite -gravity center shadow.png "' + dest_path + f + ' " "' + dest_path + f + '"')
+            print 'Generate "' + dest_path + f + '"'
         elif ".jpg" in f:
             run_cmd('convert "' + source_path + f + '" -resize ' + resize + ' "./' + dest_path + f.replace("jpg","png") + '"')
-            print 'convert "' + source_path + f + '" -resize ' + resize + ' "./' + dest_path + f.replace("jpg","png") + '"'
-
+            print 'Generate "' + dest_path + f + '"'
+'''
 convert kof97-snap.png -resize 1280x400\! bg.png -composite kof97.png
 convert kof97-snap.png -resize 1280x400^ bg.png -composite kof97.png
 convert kof97-logo.png -resize 1280x400 -resize 80% logo.png 
 convert logo.png -background black -shadow 80x3+5+5 logo.png -composite shadow.png
 composite -gravity center shadow.png kof97.png kof97.png 
+'''

feaf64c3a724973f2d96dc2dc3af75f4c307eb56
rinalim <ooll00@naver.com>
2020-09-26 13:01:22 +0900

    Update autogen.py

--- marquee/autogen.py
+++ marquee/autogen.py
@@ -8,15 +8,18 @@ def run_cmd(cmd):
     return output
   
 input_dir = sys.argv[1].split("/")[0]
-input_file = sys.argv[1].split("/")[1]
 source_path = "/home/pi/RetroPie/roms/"+input_dir+"/marquee/"
-snapshot_parh = "/home/pi/RetroPie/roms/"+input_dir+"/snap/"
+snapshot_path = "/home/pi/RetroPie/roms/"+input_dir+"/snap/"
 dest_path = input_dir+"/"
 resize = sys.argv[2]
 resize_method = sys.argv[3]
-bg_color = sys.argv[4]
-bg_trans = str(100-int(sys.argv[5])*100)
-marquee_size = sys.argv[6]
+logo_ratio = int(sys.argv[4])
+bg_color = sys.argv[5]
+bg_trans = str(int(100-int(sys.argv[6])))
+
+logo_x = int(int(resize.split('x')[0])*logo_ratio/100)
+logo_y = int(int(resize.split('x')[1])*logo_ratio/100)
+logo_size = str(logo_x)+"x"+str(logo_y)
 
 run_cmd("convert -size " + resize + " xc:" + bg_color + 
     " -matte -channel A +level 0," + bg_trans + "% +channel ./bg.png")
@@ -26,24 +29,30 @@ if os.path.isdir(source_path) == False:
 else:
     if os.path.isdir(os.getcwd()+"/"+dest_path) == False:
         os.mkdir(os.getcwd()+"/"+dest_path)
-    if input_file == "*":
+    if dest_path == sys.argv[1]:
         file_list = os.listdir(source_path)
     else:
-        file_list = [input_file]
+        file_list = [sys.argv[1].split("/")[1]]
+    file_list.sort()
     for f in file_list:
         if ".png" in f:
-            run_cmd('convert "' + snapshot_parh + f + '" -resize ' + resize + ' "\! bg.png -composite ' + dest_path + f + '"')
-            run_cmd('convert "' + source_path + f + '" -resize ' + resize + ' logo.png')
+            if os.path.isfile(snapshot_path+f) == True:
+                if resize_method == "fill":
+                    run_cmd('convert "' + snapshot_path + f + 
+                            '" -trim -resize ' + resize + '\! bg.png -composite "' + dest_path + f + '"')
+                elif resize_method == 'crop':
+                    run_cmd('convert "' + snapshot_path + f + 
+                            '" -trim -resize ' + resize + '^ -gravity center -extent ' + resize + 
+                            ' bg.png -composite "' + dest_path + f + '"')
+                else:
+                    print 'Resize option error'
+            else:
+                run_cmd('cp bg.png ' + dest_path + f) 
+            run_cmd('convert "' + source_path + f + '" -trim -resize ' + logo_size + ' logo.png')
             run_cmd('convert logo.png -background black -shadow 80x3+5+5 logo.png -composite shadow.png')
-            run_cmd('composite -gravity center shadow.png "' + dest_path + f + ' " "' + dest_path + f + '"')
+            run_cmd('composite -gravity center shadow.png "./' + dest_path + f + '" "./' + dest_path + f + '"')
             print 'Generate "' + dest_path + f + '"'
         elif ".jpg" in f:
-            run_cmd('convert "' + source_path + f + '" -resize ' + resize + ' "./' + dest_path + f.replace("jpg","png") + '"')
+            run_cmd('convert "' + source_path + f + '" -resize ' + 
+                    resize + ' "./' + dest_path + f.replace("jpg","png") + '"')
             print 'Generate "' + dest_path + f + '"'
-'''
-convert kof97-snap.png -resize 1280x400\! bg.png -composite kof97.png
-convert kof97-snap.png -resize 1280x400^ bg.png -composite kof97.png
-convert kof97-logo.png -resize 1280x400 -resize 80% logo.png 
-convert logo.png -background black -shadow 80x3+5+5 logo.png -composite shadow.png
-composite -gravity center shadow.png kof97.png kof97.png 
-'''
